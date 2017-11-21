86c65b3378929fab66771088c3eff0beed9d5968
Vitaly Koulakov <vitaly.koulakov@firstlinesoftware.com>
2017-11-21 15:47:01 +0300

    ARACHNE-1442 Import of PLP

--- arachne-commons/src/main/java/com/odysseusinc/arachne/commons/api/v1/dto/CommonPredictionDTO.java
+++ arachne-commons/src/main/java/com/odysseusinc/arachne/commons/api/v1/dto/CommonPredictionDTO.java
@@ -3,15 +3,4 @@ package com.odysseusinc.arachne.commons.api.v1.dto;
 import java.io.Serializable;
 
 public class CommonPredictionDTO extends CommonEntityDTO implements Serializable {
-    private String modelType;
-
-    public String getModelType() {
-
-        return modelType;
-    }
-
-    public void setModelType(String modelType) {
-
-        this.modelType = modelType;
-    }
 }
