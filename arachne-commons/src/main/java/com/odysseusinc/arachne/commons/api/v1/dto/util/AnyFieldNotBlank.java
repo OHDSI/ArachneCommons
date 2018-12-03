b600fb216c050272d4573d9405ae6ffca3ad73e0
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2018-12-03 19:33:13 +0300

    Fixes Manual datanode cannot be created (#153)

---
+++ arachne-commons/src/main/java/com/odysseusinc/arachne/commons/api/v1/dto/util/AnyFieldNotBlank.java
@@ -0,0 +1,22 @@
+package com.odysseusinc.arachne.commons.api.v1.dto.util;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+/**
+ * @author vkoulakov
+ * @since 12/3/18.
+ */
+@Target({ ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = AnyFieldNotBlankValidator.class)
+public @interface AnyFieldNotBlank {
+	String message() default "{org.hibernate.validator.constraints.NotBlank.message}";
+	Class<?>[] groups() default {};
+	Class<? extends Payload>[] payload() default {};
+	String[] fields() default {"id", "name"};
+}
