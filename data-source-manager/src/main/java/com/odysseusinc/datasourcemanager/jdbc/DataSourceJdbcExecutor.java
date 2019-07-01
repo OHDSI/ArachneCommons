f966280a2185a45e70cb237adccf2ae1329c0764
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2019-07-01 17:08:19 +0300

    Common Cloud services. Introduces auth resolver mech (#190)

---
+++ data-source-manager/src/main/java/com/odysseusinc/datasourcemanager/jdbc/DataSourceJdbcExecutor.java
@@ -0,0 +1,65 @@
+package com.odysseusinc.datasourcemanager.jdbc;
+
+import com.odysseusinc.arachne.commons.types.DBMSType;
+import com.odysseusinc.arachne.execution_engine_common.api.v1.dto.DataSourceUnsecuredDTO;
+import com.odysseusinc.arachne.execution_engine_common.util.BigQueryUtils;
+import com.odysseusinc.datasourcemanager.jdbc.auth.BigQueryAuthResolver;
+import com.odysseusinc.datasourcemanager.jdbc.auth.DataSourceAuthResolver;
+import com.odysseusinc.datasourcemanager.jdbc.auth.KerberosAuthResolver;
+import com.odysseusinc.datasourcemanager.krblogin.KerberosService;
+import com.odysseusinc.datasourcemanager.krblogin.RuntimeServiceMode;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+public class DataSourceJdbcExecutor {
+
+	private final KerberosService kerberosService;
+	private List<DataSourceAuthResolver> authResolvers;
+
+	public DataSourceJdbcExecutor(KerberosService kerberosService) {
+
+		this.kerberosService = kerberosService;
+		initDefaultResolvers();
+	}
+
+	private void initDefaultResolvers() {
+
+		this.authResolvers = new ArrayList<>();
+		authResolvers.add(new BigQueryAuthResolver());
+		authResolvers.add(new KerberosAuthResolver(kerberosService));
+	}
+
+	public <T> T executeOnSource(DataSourceUnsecuredDTO dataSourceData, JdbcTemplateConsumer<T> consumer) throws IOException {
+
+		if (Objects.isNull(consumer)) {
+			throw new IllegalArgumentException("consumer is required");
+		}
+
+		File tempDir = Files.createTempDirectory("gis").toFile();
+
+		// Resolve authentication
+		authResolvers.stream().filter(r -> r.supports(dataSourceData)).forEach(r -> r.resolveAuth(dataSourceData, tempDir));
+
+		DriverManagerDataSource dataSource = new DriverManagerDataSource(
+						dataSourceData.getConnectionString(),
+						dataSourceData.getUsername(),
+						dataSourceData.getPassword()
+		);
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+		try {
+			return consumer.execute(jdbcTemplate);
+		} finally {
+			FileUtils.deleteQuietly(tempDir);
+		}
+	}
+}
