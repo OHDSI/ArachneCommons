826ac8501d581aa909a8b2f15a393c47aa9c7a77
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2019-07-01 18:39:11 +0300

    Re-uses DataSourceAuthResolver-s from arachne-commons

--- data-source-manager/src/main/java/com/odysseusinc/datasourcemanager/jdbc/auth/KerberosAuthResolver.java
+++ data-source-manager/src/main/java/com/odysseusinc/datasourcemanager/jdbc/auth/KerberosAuthResolver.java
@@ -2,14 +2,16 @@ package com.odysseusinc.datasourcemanager.jdbc.auth;
 
 import com.odysseusinc.arachne.execution_engine_common.api.v1.dto.DataSourceUnsecuredDTO;
 import com.odysseusinc.datasourcemanager.krblogin.KerberosService;
+import com.odysseusinc.datasourcemanager.krblogin.KrbConfig;
 import com.odysseusinc.datasourcemanager.krblogin.RuntimeServiceMode;
 import java.io.File;
 import java.io.IOException;
 import java.util.Objects;
+import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class KerberosAuthResolver implements DataSourceAuthResolver {
+public class KerberosAuthResolver implements DataSourceAuthResolver<KrbConfig> {
 
 	private static final Logger logger = LoggerFactory.getLogger(KerberosAuthResolver.class);
 
@@ -27,10 +29,11 @@ public class KerberosAuthResolver implements DataSourceAuthResolver {
 	}
 
 	@Override
-	public void resolveAuth(DataSourceUnsecuredDTO dataSourceData, File workDir) {
+	public Optional<KrbConfig> resolveAuth(DataSourceUnsecuredDTO dataSourceData, File workDir) {
 
 		try {
-			kerberosService.runKinit(dataSourceData, RuntimeServiceMode.SINGLE, workDir);
+			KrbConfig krbConfig = kerberosService.runKinit(dataSourceData, RuntimeServiceMode.SINGLE, workDir);
+			return Optional.of(krbConfig);
 		} catch (IOException e) {
 			logger.error("Failed to resolve Kerberos authentication for Source: {}", dataSourceData.getName(), e);
 			throw new RuntimeException(e);
