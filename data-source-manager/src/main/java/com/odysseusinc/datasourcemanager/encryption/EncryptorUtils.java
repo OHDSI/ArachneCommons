357ab01b868a7ea1794e4841ffa56baed36a734f
molodkov yaroslav <yaroslav.molodkov@odysseusinc.com>
2019-07-24 16:46:33 +0200

    Refactoring of EncryptorUtils to allow usage from other projects + unit test (#197)

--- data-source-manager/src/main/java/com/odysseusinc/datasourcemanager/encryption/EncryptorUtils.java
+++ data-source-manager/src/main/java/com/odysseusinc/datasourcemanager/encryption/EncryptorUtils.java
@@ -1,8 +1,10 @@
 package com.odysseusinc.datasourcemanager.encryption;
 
+import java.util.Objects;
 import org.apache.commons.lang3.StringUtils;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PBEStringEncryptor;
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,15 +14,18 @@ public class EncryptorUtils {
 
 	private static final Logger logger = LoggerFactory.getLogger(EncryptorUtils.class);
 
+	public static final String PREFIX = "ENC(";
+	public static final String SUFFIX = ")";
+
 	private EncryptorUtils(){}
 
-	public static StringEncryptor buildStringEncryptor(Environment env) {
+	public static PBEStringEncryptor buildStringEncryptor(Environment env) {
 
 		StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
 		encryptor.setProvider(new BouncyCastleProvider());
 		encryptor.setProviderName("BC");
 		encryptor.setAlgorithm(env.getRequiredProperty("jasypt.encryptor.algorithm"));
-		if ("PBEWithMD5AndDES".equals(env.getRequiredProperty("jasypt.encryptor.algorithm"))) {
+		if (StringUtils.equals("PBEWithMD5AndDES", env.getRequiredProperty("jasypt.encryptor.algorithm"))) {
 			logger.warn("Warning:  encryption algorithm set to PBEWithMD5AndDES, which is not considered a strong encryption algorithm.  You may use PBEWITHSHA256AND128BITAES-CBC-BC, but will require special JVM configuration to support these stronger methods.");
 		}
 		encryptor.setKeyObtentionIterations(1000);
@@ -29,5 +34,28 @@ public class EncryptorUtils {
 			encryptor.setPassword(password);
 		}
 		return encryptor;
+	}
+
+	public static String decrypt(StringEncryptor encryptor, String value) {
+
+		if (Objects.isNull(encryptor) || encryptor instanceof NotEncrypted) {
+			return value;
+		}
+		if (Objects.isNull(value) || !value.startsWith(PREFIX)) {
+			return value;
+		}
+		String encryptedData = value.substring(PREFIX.length(), value.length() - SUFFIX.length());
+		return encryptor.decrypt(encryptedData);
+	}
+
+	public static String encrypt(StringEncryptor encryptor, String value) {
+
+		if (Objects.isNull(encryptor) || encryptor instanceof NotEncrypted) {
+			return value;
+		}
+		if (Objects.isNull(value)) {
+			return value;
+		}
+		return PREFIX + encryptor.encrypt(value) + SUFFIX;
 	}
 }
