754f11354e8f7a3c59f7813eb903b3bfd1744947
PGrafkin <pavgra@gmail.com>
2019-09-03 07:54:52 -0400

    Added explicit assert

--- execution-engine-commons/src/test/java/com/odysseusinc/arachne/execution_engine_common/ConnectionParamsParserTest.java
+++ execution-engine-commons/src/test/java/com/odysseusinc/arachne/execution_engine_common/ConnectionParamsParserTest.java
@@ -1,14 +1,19 @@
 package com.odysseusinc.arachne.execution_engine_common;
 
 import com.odysseusinc.arachne.commons.types.DBMSType;
+import com.odysseusinc.arachne.execution_engine_common.util.ConnectionParams;
 import com.odysseusinc.arachne.execution_engine_common.util.ConnectionParamsParser;
 import org.junit.Test;
+import org.springframework.util.Assert;
+
+import java.util.Objects;
 
 public class ConnectionParamsParserTest {
 
     @Test
     public void testParsingParamWithEmptyValue() {
 
-        ConnectionParamsParser.parse(DBMSType.POSTGRESQL, "jdbc:postgresql://localhost:64290/postgres?binaryTransferEnable=&unknownLength=2147483647");
+        ConnectionParams connectionParams = ConnectionParamsParser.parse(DBMSType.POSTGRESQL, "jdbc:postgresql://localhost:64290/postgres?binaryTransferEnable=&unknownLength=2147483647");
+        Assert.isTrue(Objects.equals(connectionParams.getExtraSettings(), "binaryTransferEnable=&unknownLength=2147483647"), "Extra params parsed properly");
     }
 }
